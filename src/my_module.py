0cd8cf28597e28b567962490916458c749a744af
Jorge Henrique Ferreira da Silva <68034513+Jhferr@users.noreply.github.com>
2022-05-08 23:32:22 -0300

    Programa de Estágio Syngenta Digital 2022
    Implementação em python do Problema da Reserva de Hotel em "my_module.py".

--- src/my_module.py
+++ src/my_module.py
@@ -1,3 +1,43 @@
-def get_cheapest_hotel(number):   #DO NOT change the function's name
-    cheapest_hotel = "cheapest_hotel_name"
-    return cheapest_hotel
+def get_cheapest_hotel(request):
+    hotel = ['Lakewood', 'Bridgewood', 'Ridgewood']
+    classification = [3, 4, 5]
+    daysOfTheWeek = {'sun': 1, 'mon': 0, 'tues': 0, 'wed': 0, 'thur': 0, 'fri': 0, 'sat': 1}
+
+    weekdayRates = [110, 160, 220]
+    weekendRates = [90, 60, 150]
+    regularCostumer = [weekdayRates, weekendRates, classification]
+
+    weekdayRates = [80, 110, 100]
+    weekendRates = [80, 50, 40]
+    rewardCostumer = [weekdayRates, weekendRates, classification]  
+
+    information = request.replace(':',' ').replace(',',' ').split()
+    costumerType = information[0]
+    listOfDates = information[1:] 
+    prices = [0, 0, 0]
+    daysBooking = [0, 0]  
+    # calculo da quantidade de dias da semana e do fim de semana sendo agendado
+    for i in listOfDates:   
+        _, day = i.replace('(',' ').split()
+        day = day[:-1]
+        daysBooking[daysOfTheWeek[day]] += 1
+
+    if (costumerType == 'Regular'):
+        prices[0] = (regularCostumer[0][0] * daysBooking[0]) + (regularCostumer[1][0] * daysBooking[1])
+        prices[1] = (regularCostumer[0][1] * daysBooking[0]) + (regularCostumer[1][1] * daysBooking[1])
+        prices[2] = (regularCostumer[0][2] * daysBooking[0]) + (regularCostumer[1][2] * daysBooking[1])
+    elif (costumerType == 'Rewards'):
+        prices[0] = (rewardCostumer[0][0] * daysBooking[0]) + (rewardCostumer[1][0] * daysBooking[1])
+        prices[1] = (rewardCostumer[0][1] * daysBooking[0]) + (rewardCostumer[1][1] * daysBooking[1])
+        prices[2] = (rewardCostumer[0][2] * daysBooking[0]) + (rewardCostumer[1][2] * daysBooking[1])
+    else:
+        print('Tipo de cliente desconhecido.')
+        raise ValueError
+
+    hotelsInfo = [0, 0, 0]
+    for i in range(3):
+        hotelsInfo[i] = [hotel[i], prices[i], -classification[i]]
+    # ordenamento com preço como prioridade e classificação em segundo lugar 
+    hotelsInfo.sort(key = lambda lis: (lis[1], lis[2])) 
+    
+    return hotelsInfo[0][0]  
